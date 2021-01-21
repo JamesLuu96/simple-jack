eeb7a94c8a6aaa3be7979a639db7a924e5751408
fasikaWalle <fasikabini12@gmail.com>
2021-01-21 15:04:21 -0600

    html play added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,34 @@
-var btn = document.querySelector("#btnModal");
-var modal = document.querySelector(".modal");
-var modalbg = document.querySelector(".modal-background");
-var btnClose = document.querySelector("#btn-close");
 
-//pop up the rules modal
-btn.addEventListener("click", () => {
-  modal.classList.add("is-active");
-});
+$(document).ready(function () {
 
-//close the rules modals
-modalbg.addEventListener("click", () => {
-  modal.classList.remove("is-active");
-});
-btnClose.addEventListener("click", () => {
-  modal.classList.remove("is-active");
-});
 
-//
-var btnPlay = document.querySelector("#play");
 
-$("#play").on("click", function () {
-  $(".home").hide();
+
+
+  var btn = document.querySelector("#btnModal");
+  var modal = document.querySelector(".modal");
+  var modalbg = document.querySelector(".modal-background");
+  var btnClose = document.querySelector("#btn-close");
+
+  //pop up the rules modal
+  btn.addEventListener("click", () => {
+    modal.classList.add("is-active");
+  });
+
+  //close the rules modals
+  modalbg.addEventListener("click", () => {
+    modal.classList.remove("is-active");
+  });
+  btnClose.addEventListener("click", () => {
+    modal.classList.remove("is-active");
+  });
+  $(".play-section").hide();
+  var btnPlay = document.querySelector("#play");
+
+  $("#play").on("click", function () {
+    $(".home").hide();
+    $(".play-section").show();
+  })
+
+
 })
