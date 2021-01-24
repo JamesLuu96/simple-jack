c97f05db52668f4689de68c7ebd57b08067c0fde
Pure KaoZ <nickolaus.carter81@gmail.com>
2021-01-24 14:16:09 -0600

    change character to global variable

--- assets/js/prizes.js
+++ assets/js/prizes.js
@@ -1,6 +1,8 @@
 const gateway = 'https://gateway.marvel.com';
-const apiKey = '&apikey=6183cc1410bb4bd83659bc716cd7fadb';
+const apiKey = 'apikey=6183cc1410bb4bd83659bc716cd7fadb';
 // use this for list of available series and to make calls to api
+var amazingSpiderMan = localStorage.getItem('amazingSpiderMan') || [];
+var characters = [];
 const selections = [
   {
     'series': 'Amazing Spider-Man',
@@ -72,8 +74,31 @@ const selections = [
   }  
 ];
 
-// TODO: check available characters against unlocked characters
+async function seriesList() {
+  var seriesSearch = 'https://gateway.marvel.com/v1/public/series/';
+  var charLimit = 'characters?limit=100';
 
+  for (let i = 0; i < selections.length; i++) {
+
+    var response = await fetch(`${seriesSearch}/${selections[i].id}/${charLimit}&${apiKey}`);
+    var data = await response.json();
+
+    for (let i = 0; i < data.data.results.length; i++) {
+
+      var picPath = data.data.results[i].thumbnail.path;
+
+      if (picPath.includes('image_not_available')) {
+        continue;
+      } else {
+        // var newChar = data.data.results[i].id;
+        // var newArr = newChar.filter()
+        characters.push(data.data.results[i].id);
+      }
+    }
+  }
+};
+
+// TODO: check available characters against unlocked characters
 function checkCharacters() {
   unlockedChars = localStorage.getItem('name') || [];
 
@@ -101,82 +126,103 @@ function pageBuild() {
   seriesDisplay();
 };
 
-// display series based on option selection (default is Amazing Spider-Man)
-async function seriesDisplay(value) {
+async function seriesDisplay() {
   // API limits query results to 100 and defaults to 20 unless otherwise specified
-  var seriesComp = '/v1/public/series/454/characters?limit=100';
-  // clear previous series
-  $('.shop-container').empty()
+  var charSearch = '/v1/public/characters/';
   // will skip on initial page load and load default series list
-  if (value) {
-    seriesComp = seriesComp.replace('454', value);
-    console.log(seriesComp)
-  }
+  for (let i = 0; i < characters.length; i++) {
+    
+    var response = await fetch(`${gateway}${charSearch}${characters[i]}?${apiKey}`);
+    var data = await response.json();
+    var name = (data.data.results[0].name);
+    var picPath = data.data.results[0].thumbnail.path;
+    var picExtension = data.data.results[0].thumbnail.extension;
 
-  var response = await fetch(`${gateway}${seriesComp}${apiKey}`);
-  var data = await response.json();
-  // debugger
-
-  for (let i = 0; i < data.data.results.length; i++) {
-    console.log("I'm in for loop")
-    var name = (data.data.results[i].name);
-    var picPath = data.data.results[i].thumbnail.path;
-    var picExtension = data.data.results[i].thumbnail.extension;
-    if (picPath.includes('image_not_available')) {
-      continue;
-    } else {
-      var thumbnail = `${picPath}/portrait_uncanny.${picExtension}`
-      var container = $('<div>');
-      var listItem = $('<p>').text(`${name}`);
-      var picture = $('<img>').attr('src', thumbnail);
-      container.append(listItem);
-      container.append(picture);
-      if (bankMoney >= 200) {
-        var buttonContainer = $('<div>').addClass('unlock-card');
-        var button = $('<button>').addClass('button is-success').attr('title', 'you need 200 points').text('Unlock');
-        buttonContainer.append(button);
-        container.append(buttonContainer);
-      } else {
-        var notEnough = $('<p>').text('You don\'t have enough points');
-        container.append(notEnough)
-      }
-
-      $('.shop-container').append(container);
-    }
-  }
-  
-  // checks array length and calls new fetch to pull rest of results for series
-  if (data.data.results.length === 100) {
-    seriesComp += '&offset=100';
-    fetch(`${gateway}${seriesComp}${apiKey}`).then(function(response) {
-      response.json().then(function(data) {
-        for (let i = 0; i < data.data.results.length; i++) {
-          var name = (data.data.results[i].name);
-          var picPath = data.data.results[i].thumbnail.path;
-          var picExtension = data.data.results[i].thumbnail.extension;
-          if (picPath.includes('image_not_available')) {
-            continue;
-          } else {
-            var thumbnail = `${picPath}/portrait_uncanny.${picExtension}`
-            var container = $('<div>');
-            var listItem = $('<p>').text(`${name}`);
-            var picture = $('<img>').attr('src', thumbnail);
-            container.append(listItem);
-            container.append(picture);
-            var buttonContainer = $('<div>').addClass('unlock-card');
-            var button = $('<button>').addClass('button is-success').attr('title', 'you need 200 points').text('Unlock');
-            buttonContainer.append(button);
-            container.append(buttonContainer);
-
-            $('.shop-container').append(container);
-          }
-        }
-      })
-    })
+    var thumbnail = `${picPath}/portrait_uncanny.${picExtension}`
+    var container = $('<div>');
+    var listItem = $('<p>').text(`${name}`);
+    var picture = $('<img>').attr('src', thumbnail);
+    container.append(listItem);
+    container.append(picture);
+    $('.shop-container').append(container);
+    // debugger
   }
 };
 
-// add pictures to local array of objects
+// display series based on option selection (default is Amazing Spider-Man)
+// async function seriesDisplay(value) {
+//   // API limits query results to 100 and defaults to 20 unless otherwise specified
+//   var seriesComp = '/v1/public/series/454/characters?limit=100';
+//   // clear previous series
+//   $('.shop-container').empty()
+//   // will skip on initial page load and load default series list
+//   if (value) {
+//     seriesComp = seriesComp.replace('454', value);
+//     console.log(seriesComp)
+//   }
+
+//   var response = await fetch(`${gateway}${seriesComp}${apiKey}`);
+//   var data = await response.json();
+//   // debugger
+
+//   for (let i = 0; i < data.data.results.length; i++) {
+//     // console.log(data)
+//     var name = (data.data.results[i].name);
+//     var picPath = data.data.results[i].thumbnail.path;
+//     var picExtension = data.data.results[i].thumbnail.extension;
+//     if (picPath.includes('image_not_available')) {
+//       continue;
+//     } else {
+//       var thumbnail = `${picPath}/portrait_uncanny.${picExtension}`
+//       var container = $('<div>');
+//       var listItem = $('<p>').text(`${name}`);
+//       var picture = $('<img>').attr('src', thumbnail);
+//       container.append(listItem);
+//       container.append(picture);
+//       if (bankMoney >= 200) {
+//         var buttonContainer = $('<div>').addClass('unlock-card');
+//         var button = $('<button>').addClass('button is-success').attr('title', 'you need 200 points').text('Unlock');
+//         buttonContainer.append(button);
+//         container.append(buttonContainer);
+//       } else {
+//         var notEnough = $('<p>').text('You don\'t have enough points');
+//         container.append(notEnough)
+//       }
+
+//       $('.shop-container').append(container);
+//     }
+//   }
+  
+//   // checks array length and calls new fetch to pull rest of results for series
+//   if (data.data.results.length === 100) {
+//     seriesComp += '&offset=100';
+//     fetch(`${gateway}${seriesComp}${apiKey}`).then(function(response) {
+//       response.json().then(function(data) {
+//         for (let i = 0; i < data.data.results.length; i++) {
+//           var name = (data.data.results[i].name);
+//           var picPath = data.data.results[i].thumbnail.path;
+//           var picExtension = data.data.results[i].thumbnail.extension;
+//           if (picPath.includes('image_not_available')) {
+//             continue;
+//           } else {
+//             var thumbnail = `${picPath}/portrait_uncanny.${picExtension}`
+//             var container = $('<div>');
+//             var listItem = $('<p>').text(`${name}`);
+//             var picture = $('<img>').attr('src', thumbnail);
+//             container.append(listItem);
+//             container.append(picture);
+//             var buttonContainer = $('<div>').addClass('unlock-card');
+//             var button = $('<button>').addClass('button is-success').attr('title', 'you need 200 points').text('Unlock');
+//             buttonContainer.append(button);
+//             container.append(buttonContainer);
+
+//             $('.shop-container').append(container);
+//           }
+//         }
+//       })
+//     })
+//   }
+// };
 
 $('#select-box').change((event) => {
 
@@ -184,61 +230,11 @@ $('#select-box').change((event) => {
   seriesDisplay(value);
 })
 
+// TODO: target unlock button to add character to localStorage
 $('.unlock-card').on('click', () => {
   var name = $(this).closest('div').find('p').text()
   console.log(name)
 })
 
+seriesList();
 pageBuild();
-// throw away - just to pull the list of series, IDs, and available characters
-
-// Amazing Spider-Man (1999 - 2013) - series 454	- total characters 145
-// Black Panther (2005 - 2008) - series 784	- total characters	31
-// Blade (2006 - 2007) - series 1123	- total characters	5
-// Captain America (1998 - 2002) - series 1997	- total characters	31
-// Captain Marvel (2012 - 2013) - series 16280	- total characters	1
-// Daredevil (2011 - 2014) - series 12916	- total characters  12
-// Deadpool (2008 - 2012) - series 5701	- total characters	15
-// Power Man and Iron Fist (2016 - 2017) - series 21122	- total characters	7
-// Punisher 2099 (1993) - series 20020	- total characters	2
-// Spider-Verse (2014 - 2015) - series 18892	- total characters	4
-// Thor (2007 - 2011) - series 2473	- total characters	12
-// Wolverine Origins (2006 - 2010) - series 2375	- total characters	41
-// Extraordinary X-Men (2015 - 2017) - series 20460	- total characters	8
-// Fantastic Four (2012 - 2014) - series 16557	- total characters	7
-// Luke Cage (2017 - 2018) - series 23045	- total characters	2
-// Marvel Adventures the Avengers (2006 - 2009) - series 1107	- total characters	10
-// New X-Men (2004 - 2008) - series 749	- total characters	29
-// total characters from all series	362
-
-// async function testFunction1() {
-
-//   var response = await fetch(`${gateway}${seriesComp}&offset=100${apiKey}`)
-//   var data = await response.json()
-//         // debugger
-
-        
-//         for (let i = 0; i < 100; i++) {
-//           var title = data.data.results[i].title;
-//           var id = data.data.results[i].id;
-//           var characters = data.data.results[i].characters.available;
-//           var listItem = $('<p>').text(`${title} - ${id} - ${characters}`)
-//           $('#test').append(listItem);
-//         }
-// };
-
-// async function testFunction2() {
-
-//   var response = await fetch(`${gateway}${seriesComp}&offset=200${apiKey}`)
-//   var data = await response.json()
-//         // debugger
-
-        
-//         for (let i = 0; i < 100; i++) {
-//           var title = data.data.results[i].title;
-//           var id = data.data.results[i].id;
-//           var characters = data.data.results[i].characters.available;
-//           var listItem = $('<p>').text(`${title} - ${id} - ${characters}`)
-//           $('#test').append(listItem);
-//         }
-// };
